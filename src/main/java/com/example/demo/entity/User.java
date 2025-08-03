b56a2ec7f75233409a46e122596bf48438f7fb14
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-08-03 13:06:51 +0200

    Security Config

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    @ManyToOne
+    private Role role;
+}
