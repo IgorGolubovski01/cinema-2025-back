1a18254726c3b8d6e6d3556a5423585dd972f24c
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-08-05 21:16:59 +0200

    .

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.demo.dto.SignUpRequest;
 import com.example.demo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -18,7 +15,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("signUp")
-    public ResponseEntity<String> signUp(SignUpRequest request){
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequest request){
         return userService.signUp(request);
     }
 }
